7b9121213106691c11c6dd270c668e3158736dc9
Vladyslav Marenych <marenich2002@gmail.com>
2025-09-13 00:21:43 +0300

    Add project files.

---
+++ MonoGameLibrary/Core.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameLibrary
+{
+    public class Core: Game
+    {
+        internal static Core s_instance;
+
+        public static Core Instance => s_instance;
+
+        public static GraphicsDeviceManager Graphics { get; private set; }
+
+        public static new GraphicsDevice GraphicsDevice { get; private set; }
+
+        public static SpriteBatch SpriteBatch { get; private set; }
+
+        public static new ContentManager Content { get; private set; }
+
+        public Core(string title, int width, int height, bool fullScreen)
+        {
+            if (s_instance != null)
+            {
+                throw new InvalidOperationException($"Only a single Core instance can be created");
+            }
+
+            s_instance = this;
+
+            Graphics = new GraphicsDeviceManager(this);
+
+            Graphics.PreferredBackBufferWidth = width;
+            Graphics.PreferredBackBufferHeight = height;
+            Graphics.IsFullScreen = fullScreen;
+
+            Window.Title = title;
+
+            Content = base.Content;
+
+            Content.RootDirectory = "Content";
+
+            IsMouseVisible = true;
+
+            Graphics.ApplyChanges();
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            GraphicsDevice = base.GraphicsDevice;
+
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+    }
+}
