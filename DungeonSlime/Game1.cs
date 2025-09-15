4b2b190a3d7f73590b760b33ffee3f2352bb23b4
Vladyslav Marenych <marenich2002@gmail.com>
2025-09-15 20:57:58 +0300

    Add collision detection

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -18,6 +18,10 @@ public class Game1 : Core
 
     private const float MOVEMENT_SPEED = 5.0f;
 
+    private Vector2 _batPosition;
+
+    private Vector2 _batVelocity;
+
     public Game1() : base("Dungen Slime",1280,720,false )
     {
 
@@ -25,9 +29,11 @@ public class Game1 : Core
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
+
+        _batPosition = new Vector2(_slime.Width + 10, 0);
+
+        AssignRandomBatVelocity();
     }
 
     protected override void LoadContent()
@@ -54,7 +60,102 @@ public class Game1 : Core
 
         CheckGamePadInput();
 
+        Rectangle screenBounds = new Rectangle(
+            0,
+            0,
+            GraphicsDevice.PresentationParameters.BackBufferWidth,
+            GraphicsDevice.PresentationParameters.BackBufferHeight
+        );
+
+        Circle slimeBounds = new Circle(
+            (int)(_slimePosition.X + (_slime.Width * 0.5f)),
+            (int)(_slimePosition.Y + (_slime.Height * 0.5f)),
+            (int)(_slime.Width * 0.5f)
+        );
+
+        if (slimeBounds.Left < screenBounds.Left)
+        {
+            _slimePosition.X = screenBounds.Left;
+        }
+        else if (slimeBounds.Right > screenBounds.Right)
+        {
+            _slimePosition.X = screenBounds.Right - _slime.Width;
+        }
+
+        if (slimeBounds.Top < screenBounds.Top)
+        {
+            _slimePosition.Y = screenBounds.Top;
+        }
+        else if (slimeBounds.Bottom > screenBounds.Bottom)
+        {
+            _slimePosition.Y = screenBounds.Bottom - _slime.Height;
+        }
+
+        Vector2 newBatPosition = _batPosition + _batVelocity;
+
+        Circle batBounds = new Circle(
+            (int)(newBatPosition.X + (_bat.Width * 0.5f)),
+            (int)(newBatPosition.Y + (_bat.Height * 0.5f)),
+            (int)(_bat.Width * 0.5f)
+        );
+
+        Vector2 normal = Vector2.Zero;
+
+        if (batBounds.Left < screenBounds.Left)
+        {
+            normal.X = Vector2.UnitX.X;
+            newBatPosition.X = screenBounds.Left;
+        }
+        else if (batBounds.Right > screenBounds.Right)
+        {
+            normal.X = -Vector2.UnitX.X;
+            newBatPosition.X = screenBounds.Right - _slime.Width;
+        }
+
+        if (batBounds.Top < screenBounds.Top)
+        {
+            normal.Y = Vector2.UnitY.Y;
+            newBatPosition.Y = screenBounds.Top;
+        }
+        else if (batBounds.Bottom > screenBounds.Bottom)
+        {
+            normal.Y = -Vector2.UnitY.Y;
+            newBatPosition.Y = screenBounds.Bottom - _slime.Height;
+        }
+
+        if (normal != Vector2.Zero)
+        {
+            normal.Normalize();
+            _batVelocity = Vector2.Reflect(_batVelocity, normal);
+        }
+
+        _batPosition = newBatPosition;
+
+        if (slimeBounds.Intersects(batBounds))
+        {
+            int totalColums = GraphicsDevice.PresentationParameters.BackBufferWidth / (int)_bat.Width;
+            int totalRows = GraphicsDevice.PresentationParameters.BackBufferHeight / (int)_bat.Height;
+
+            int column = Random.Shared.Next(0, totalColums);
+            int row = Random.Shared.Next(0, totalRows);
+
+            _batPosition = new Vector2(column * _bat.Width, row * _bat.Height);
+
+            AssignRandomBatVelocity();
+        }
+
         base.Update(gameTime);
+    }
+
+    private void AssignRandomBatVelocity()
+    {
+        float angle = (float)(Random.Shared.NextDouble() * Math.PI * 2);
+
+        float x = (float)Math.Cos(angle);
+        float y = (float)Math.Sin(angle);
+        Vector2 direction = new Vector2(x, y);
+
+        _batVelocity = direction * MOVEMENT_SPEED;
     }
 
     private void CheckKeyboardInput()
@@ -138,7 +239,7 @@ public class Game1 : Core
 
         _slime.Draw(SpriteBatch, _slimePosition);
 
-        _bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
+        _bat.Draw(SpriteBatch, _batPosition);
 
         SpriteBatch.End();
 
